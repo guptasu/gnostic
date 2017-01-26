459cb9e93d0800683bda28dbdfac0687dd0783d1
guptasu <guptasu@google.com>
2017-01-26 14:55:26 -0800

    Cleanup

--- openapivendorext/sample/INSTALL_SAMPLE.sh
+++ openapivendorext/sample/INSTALL_SAMPLE.sh
@@ -25,23 +25,51 @@ go install
 
 
 
-# For every new extension
+# Now generate sample extension plugins and install them.
 #
 #
-
-# run the tool to create your own vendor extension proto and compiler
 pushd $GOPATH/src/github.com/googleapis/openapi-compiler/openapivendorext
-openapivendorextc sample/x-google-extensions.json --out_dir_relative_to_gopath_src=github.com/googleapis/openapi-compiler/openapivendorext/sample/generated/sample_x_google_extension_plugin --extension_name_to_message=x-book:Book --extension_name_to_message=x-shelve:Shelve --proto_option_suffix=GoogleExtensions
 
-# build and install your own vendor extension proto and compiler.
-# This will allow openapic to discover your vendor extension handler
-# and use it to create richer OpenAPI protobufs.
-cd $GOPATH/src/github.com/googleapis/openapi-compiler/openapivendorext/sample/generated/sample_x_google_extension_plugin/proto
-protoc --go_out=Mgoogle/protobuf/any.proto=github.com/golang/protobuf/ptypes/any:. *.proto
-go install
+    # For Google Extension Example
+    #
+    #
+    GOOGLE_EXTENSION_SCHEMA="sample/x-google-extensions.json"
+    GOOGLE_EXTENSION_OUT_DIR="github.com/googleapis/openapi-compiler/openapivendorext/sample/generated/sample_x_google_extension_plugin"
+    GOOGLE_EXTENSION_PROTO_NAMESPACE="GoogleExtensions"
 
-cd $GOPATH/src/github.com/googleapis/openapi-compiler/openapivendorext/sample/generated/sample_x_google_extension_plugin
-go install
+    openapivendorextc $GOOGLE_EXTENSION_SCHEMA --out_dir_relative_to_gopath_src=$GOOGLE_EXTENSION_OUT_DIR \
+    --proto_option_suffix=$GOOGLE_EXTENSION_PROTO_NAMESPACE \
+    --extension_name_to_message=x-book:Book \
+    --extension_name_to_message=x-shelve:Shelve \
 
+    pushd $GOPATH/src/$GOOGLE_EXTENSION_OUT_DIR/proto
+        protoc --go_out=Mgoogle/protobuf/any.proto=github.com/golang/protobuf/ptypes/any:. *.proto
+        go install
+    popd
+
+    pushd  $GOPATH/src/$GOOGLE_EXTENSION_OUT_DIR
+        go install
+    popd
+
+    # For IBM Extension Example
+    #
+    #
+    IBM_EXTENSION_SCHEMA="sample/x-ibm-extensions.json"
+    IBM_EXTENSION_OUT_DIR="github.com/googleapis/openapi-compiler/openapivendorext/sample/generated/sample_x_ibm_extension_plugin"
+    IBM_EXTENSION_PROTO_NAMESPACE="IbmExtensions"
+
+    openapivendorextc $IBM_EXTENSION_SCHEMA --out_dir_relative_to_gopath_src=$IBM_EXTENSION_OUT_DIR \
+    --proto_option_suffix=$IBM_EXTENSION_PROTO_NAMESPACE \
+    --extension_name_to_message=x-ibm-book:IbmBook \
+    --extension_name_to_message=x-ibm-shelve:IbmShelve \
+
+    pushd $GOPATH/src/$IBM_EXTENSION_OUT_DIR/proto
+        protoc --go_out=Mgoogle/protobuf/any.proto=github.com/golang/protobuf/ptypes/any:. *.proto
+        go install
+    popd
+
+    pushd $GOPATH/src/$IBM_EXTENSION_OUT_DIR
+        go install
+    popd
 
 popd
